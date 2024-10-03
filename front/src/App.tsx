be57dbf855b75b0e56e16446037cb11cdeba5e82
basim <bassim.el-sayed@epitech.eu>
2024-10-03 10:02:58 +0200

    map 2

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -3,7 +3,6 @@ import SimpleMap from "@/map.tsx";
 function App() {
     return (
         <div>
-            <h1>My Leaflet.js and React Map</h1>
             <SimpleMap/>
         </div>
     );
--- front/src/map.tsx
+++ front/src/map.tsx
@@ -1,14 +1,30 @@
 import {useRef} from "react";
 import {MapContainer, TileLayer} from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import L from "leaflet";
 
 const SimpleMap = () => {
     const mapRef = useRef(null);
     const latitude = 43.2965;
     const longitude = 5.3698;
-    
+
+    const bounds = L.latLngBounds(
+        [43.1, 5.1],
+        [43.5, 5.6]
+    );
+
     return (
-        <MapContainer center={[latitude, longitude]} zoom={13} ref={mapRef} style={{height: "100vh", width: "100vw"}}>
+        <MapContainer
+            center={[latitude, longitude]}
+            zoom={13}
+            minZoom={13}
+            maxBounds={bounds}
+            maxBoundsViscosity={1.0}
+            ref={mapRef}
+            style={{height: "100vh", width: "100vw"}}
+            scrollWheelZoom={true}
+            touchZoom={true}
+        >
             <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
