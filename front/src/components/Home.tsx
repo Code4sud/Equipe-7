753173f4b910fcb329cb9e69052b6911335a2ea8
JohnnyJohnnyJohn <JohnnyJohnnyJohn@github.com>
2024-10-04 11:07:51 +0200

    last last last

--- front/src/components/Home.tsx
+++ front/src/components/Home.tsx
@@ -4,7 +4,7 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import OverviewSection from "@/components/overView";
 import PollutionChart from '@/components/PollutionChart';
 import SimpleMap from '@/Map';
-import Chiffre from "@/components/Chiffre"
+import Chiffre from "@/components/ui/Chiffre"
 
 const MotionCard = motion(Card)
 
